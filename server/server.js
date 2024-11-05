eec3d77f4f74023d700103d4e992e7e18d54a937
niyakopcheva <niyakopcheva@gmail.com>
2024-11-05 14:22:56 +0200

    Update server.js
    Connect to database. Create post request to add songs to the database

--- server/server.js
+++ server/server.js
@@ -35,7 +35,27 @@ db.connect((err) => {
 });
 */
 
+app.post('/add-song', (req, res) => {
+  const {title, artist_id, album, duration, file_path} = req.body;
+
+  const query = `INSERT INTO songs (title, artist_id, album, duration, file_path) VALUES (?, ?, ?, ?, ?)`;
+  const values = [title, artist_id, album, duration, file_path];
+
+  db.query(query, values, (err, result) => {
+    if (err) {
+        res.status(500).json({ error: 'Failed to add song', message: err.message });
+    } else {
+        res.status(201).json({ message: 'Song added successfully', songId: result.insertId });
+    }
+  });
+  
+});
+
+
+
 // Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
