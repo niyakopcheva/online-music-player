488fd2186d45cffbba967b567c374fdcfe78b3e6
niyakopcheva <niyakopcheva@gmail.com>
2024-11-19 16:02:27 +0200

    Routing and Authentication
    Set up routing in App.js using react-router-dom. Set up firebase development and prod environments and firebase authentication in development.

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,21 @@
 
 import { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
-import getArtistName from './queries/getArtistName';
-import getProfilePic from './queries/getProfilePic';
+import Home from './components/Home';
+
 
 function App() {
-  const [artists, setArtists] = useState([]);
-  const [songs, setSongs] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/artists")
-    .then(res => res.json())
-    .then(data => setArtists(data))
-
-    fetch("http://localhost:5000/songs")
-    .then(res => res.json())
-    .then(data => setSongs(data))
-  }
-    ,[]
-  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Songs</h1>
-        <ul>
-        {songs.map((song) => (
-          <li key={song.song_id}>
-            <strong>{song.title}</strong> <br></br>
-            <img src={getProfilePic(artists, song.artist_id)}></img> {getArtistName(artists, song.artist_id)} <br></br>
-            <audio controls autoplay>
-              <source src={song.file_path} type='audio/mpeg'></source>
-              Your browser does not support the audio element.
-            </audio>
-          </li>
-        ))
-
-        }
-      </ul>
-      </header>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/'  element={ <Home/> } />
+        <Route path='/sign-in' />
+      </Routes>
+    </Router>
   );
+  
+  
 }
 
 export default App;
---
+++ client/src/components/Home.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import getArtistName from '../queries/getArtistName';
+import getProfilePic from '../queries/getProfilePic';
+
+
+export default function Home() {
+    const [artists, setArtists] = useState([]);
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/artists")
+    .then(res => res.json())
+    .then(data => setArtists(data))
+
+    fetch("http://localhost:5000/songs")
+    .then(res => res.json())
+    .then(data => setSongs(data))
+  }
+    ,[]
+  )
+  return (
+    <>
+    
+
+    <div className="App">
+      <header className="App-header">
+        <h1>Songs</h1>
+        <ul>
+        {songs.map((song) => (
+          <li key={song.song_id}>
+            <strong>{song.title}</strong> <br></br>
+            <img src={getProfilePic(artists, song.artist_id)}></img> {getArtistName(artists, song.artist_id)} <br></br>
+            <audio controls autoPlay>
+              <source src={song.file_path} type='audio/mpeg'></source>
+              Your browser does not support the audio element.
+            </audio>
+          </li>
+        ))
+
+        }
+      </ul>
+      </header>
+      
+    </div>
+
+    </>
+  );
+}
